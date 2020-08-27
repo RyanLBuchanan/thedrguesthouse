76710edd6823ebf0aa071bace85ffbdded3eb0db
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-08-27 09:50:09 -0600

    Image replace

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,14 @@
 document.querySelector("html").onclick = function() {
   alert("Sorry, I'm not quite functional, yet. Just give me a week or two, please.");
 }
+
+var gsthseImage = document.querySelector('img');
+
+gsthseImage.onclick = function () {
+  var myImages = gsthseImage.getAttribute('src');
+  if(myImages === 'images/bdrm_on.jpg') {
+    gsthseImage.setAttribute ('src', 'images/lvgrm_ktch.jpg');
+  } else {
+    gsthseImage.setAttribute ('src', 'images/bdrm_rt.jpg');
+  }
+}
