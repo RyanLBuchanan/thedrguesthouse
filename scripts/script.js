daaa744939dc04f1f7390ebbe74aa783438690b3
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-08-27 09:10:47 -0600

    Added script.js

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+// var myHeading = document.querySelector('h1')
+//
+// myHeading.textContent = 'Living the Dream!';
+
+document.querySelector("html").onclick = function() {
+  alert("Sorry, I'm not quite functional, yet. Just give me a week or to, please.");
+}
