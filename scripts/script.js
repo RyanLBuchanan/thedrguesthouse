f7fc0f1335859f9099c05f0afa58b59eb4c29e1d
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-08-27 11:10:45 -0600

    Image directory change

--- scripts/script.js
+++ scripts/script.js
@@ -8,9 +8,25 @@ var splashImage = document.querySelector('img');
 
 splashImage.onclick = function() {
   var myImages = splashImage.getAttribute('src');
-  if(myImages === 'images/bdrm_rt.jpg') {
-    splashImage.setAttribute ('src', 'images/lvgrm_ktch.jpg');
+  if(myImages === 'images/gsthse_images/bdrm_rt.jpg') {
+    splashImage.setAttribute ('src', 'images/gsthse_images/lvgrm_ktch.jpg');
   } else {
-    splashImage.setAttribute ('src', 'images/bdrm_rt.jpg');
+    splashImage.setAttribute ('src', 'images/gsthse_images/bdrm_rt.jpg');
   }
 }
+
+
+// var images = 3;
+// var arrayImagesElement = document.getElementById("arrayImages");
+//
+// function createImageNode() {
+//     var img = document.createElement('img');
+//     img.src = "https://insider.directv.com/wp-content/uploads/2012/07/nemo_big.jpg";
+//     img.width = "300";
+//     img.style.margin = "15px";
+//     return img;
+// }
+//
+// for(var i = 0; i < images; i++){
+//     arrayImagesElement.appendChild(createImageNode());
+// };
