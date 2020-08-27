09c78abfb928a0f0d057f1e664845666adb42f97
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-08-27 09:56:14 -0600

    Image functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,16 @@
-// var myHeading = document.querySelector('h1')
-//
-// myHeading.textContent = 'Living the Dream!';
+/* global document */
 
-document.querySelector("html").onclick = function() {
-  alert("Sorry, I'm not quite functional, yet. Just give me a week or two, please.");
-}
+// document.querySelector("html").onclick = function() {
+//   alert("Sorry, I'm not quite functional, yet. Just give me a week or two, please.");
+// }
 
-var gsthseImage = document.querySelector('img');
+var splashImage = document.querySelector('img');
 
-gsthseImage.onclick = function () {
-  var myImages = gsthseImage.getAttribute('src');
-  if(myImages === 'images/bdrm_on.jpg') {
-    gsthseImage.setAttribute ('src', 'images/lvgrm_ktch.jpg');
+splashImage.onclick = function() {
+  var myImages = splashImage.getAttribute('src');
+  if(myImages === 'images/bdrm_rt.jpg') {
+    splashImage.setAttribute ('src', 'images/lvgrm_ktch.jpg');
   } else {
-    gsthseImage.setAttribute ('src', 'images/bdrm_rt.jpg');
+    splashImage.setAttribute ('src', 'images/bdrm_rt.jpg');
   }
 }
